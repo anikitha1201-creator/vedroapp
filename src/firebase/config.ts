5376e18dc6b3a500fee3ed2f0f84604753f98775
Ankitha. V <anikitha1201@gmail.com>
2025-12-07 04:12:40 +0000

    bro hide the api key google

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-4331708937-71fe3",
   "appId": "1:643467769164:web:906cef33051047947fcbe9",
-  "apiKey": "AIzaSyC3MbxwJUVKa85snyZ7_B8QfJgDVzaklcg",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-4331708937-71fe3.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "643467769164"
